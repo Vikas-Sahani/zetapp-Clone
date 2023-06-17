2f5a4df6b6c99c7dcfed0264a1cde4c725e99cbe
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-06-17 18:01:35 +0530

    logoLeftAlign issue, responsiveness & moseover event

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 /* Problem 👆🏼
 Parsing error: Cannot find module 'next/babel'
@@ -10,6 +11,7 @@ Require stack:
 Soulution => https://stackoverflow.com/questions/68163385/parsing-error-cannot-find-module-next-babel
 */
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
   return (
@@ -20,13 +22,31 @@ export default function Home() {
 }
 
 function NavBar() {
+  const [blue, setBlue] = useState("blue");
+
+  const setColor = function (e) {
+    e.preventdefault();
+    e.target.style.color === "blue" ? setBlue("") : setBlue("blue");
+    console.log(e.target.style.color);
+  };
   return (
     <nav>
-      <a href="">
+      <a
+        href=""
+        style={{
+          textAlign: "right",
+          width: "19rem",
+          height: "3rem",
+          display: "flex",
+          justifyContent: "start",
+        }}
+      >
         <img
           src="https://zetapp.in/_next/static/media/zet_new_logo.7adcc993.svg"
+          style={{ position: "relative", left: "0px" }}
           className="logoImg"
         />
+        abc
       </a>
       <div className="hamburgerMenu">
         <img
@@ -35,10 +55,34 @@ function NavBar() {
         />
       </div>
       <div className="menu">
-        <a href="/">Home</a>
-        <a href="/about">About Us</a>
-        <a href="/partner-with-us">Partner With Us</a>
-        <a href="/blog">Blog</a>
+        <a
+          href="/"
+          style={{ color: `${blue}`, padding: "0rem 1rem" }}
+          onMouseover={(e) => setColor}
+        >
+          Home
+        </a>
+        <a
+          href="/about"
+          style={{ color: "", padding: "0rem 1rem" }}
+          onMouseover={(e) => setColor}
+        >
+          About Us
+        </a>
+        <a
+          href="/partner-with-us"
+          style={{ color: "", padding: "0rem 1rem" }}
+          onMouseover={(e) => setColor}
+        >
+          Partner With Us
+        </a>
+        <a
+          href="/blog"
+          style={{ color: "", padding: "0rem 1rem" }}
+          onMouseover={(e) => setColor}
+        >
+          Blog
+        </a>
         <div className="dwnldZET" style={{ margin: "10px" }}>
           <button>Download ZET</button>
         </div>
